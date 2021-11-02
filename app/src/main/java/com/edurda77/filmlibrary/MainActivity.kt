6963d7bb565ef96e60c69eba28f3328c967faede
HayarovEd <edurd@rambler.ru>
2021-11-02 09:45:36 +0500

    add menu

--- app/src/main/java/com/edurda77/filmlibrary/MainActivity.kt
+++ app/src/main/java/com/edurda77/filmlibrary/MainActivity.kt
@@ -2,10 +2,36 @@ package com.edurda77.filmlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import android.content.Intent
 
+
+
+
+private var toolbar: Toolbar? = null
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setToolbar()
+    }
+    fun setToolbar() {
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() == R.id.action_search){
+        val intent = Intent(this, SearchUsersActivity::class.java)
+        startActivity(intent)}
+        return super.onOptionsItemSelected(item)
     }
 }
