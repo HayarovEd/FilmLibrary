919c9d81f8b423318ec5cbfb5332fe5a98b812f1
HayarovEd <edurd@rambler.ru>
2021-11-02 17:50:36 +0500

    add adapter nowPlaiyngFilm

--- app/src/main/java/com/edurda77/filmlibrary/domain/Movie.kt
+++ app/src/main/java/com/edurda77/filmlibrary/domain/Movie.kt
@@ -2,6 +2,6 @@ package com.edurda77.filmlibrary.domain
 
 import android.media.Image
 
-abstract  class Movie (var movieTitle: String, var picture: Image, var year: Int){
+abstract   class Movie (var movieTitle: String, var picture: Image, var year: Int){
     
 }
--- app/src/main/java/com/edurda77/filmlibrary/ui/MainActivity.kt
+++ app/src/main/java/com/edurda77/filmlibrary/ui/MainActivity.kt
@@ -6,28 +6,35 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import android.content.Intent
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.edurda77.filmlibrary.R
-import com.edurda77.filmlibrary.ui.AboutActivity
-import com.edurda77.filmlibrary.ui.CustomActivity
+import com.edurda77.filmlibrary.domain.NovPlaiyngMovie
 import com.edurda77.filmlibrary.ui.SearchActivity
+import java.util.ArrayList
 
 
 private var toolbar: Toolbar? = null
 
 class MainActivity : AppCompatActivity() {
+    lateinit var movies : ArrayList<NovPlaiyngMovie>
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        var movie : NovPlaiyngMovie
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setToolbar()
-        val recyclerView: RecyclerView = findViewById(R.id.now_playng)
+        setRecycledView()
 
     }
 
     fun setToolbar() {
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+    fun setRecycledView() {
+        val recyclerView: RecyclerView = findViewById(R.id.now_playng)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = PlaiyngMovieAdapter(movies)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
