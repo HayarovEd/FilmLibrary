c7f8292f308d9e4e6bbed43e05230c20289d104f
HayarovEd <edurd@rambler.ru>
2021-11-10 20:32:59 +0500

    maked click om the movie

--- app/src/main/java/com/edurda77/filmlibrary/ui/MainActivity.kt
+++ app/src/main/java/com/edurda77/filmlibrary/ui/MainActivity.kt
@@ -14,25 +14,22 @@ import com.edurda77.filmlibrary.domain.Movie
 import android.widget.Toast
 
 
-
-
-
 private var toolbar: Toolbar? = null
 val movies: ArrayList<Movie> = ArrayList()
 
 
 class MainActivity : AppCompatActivity() {
     private var action = listOf(
-        Movie("Терминатор", "action", "120 min",10.0, 1984, 1.0, 3.3, "fgfgfgfgfg"),
-        Movie("Терминатор2", "action", "120 min",10.0, 1992, 1.4, 10.0, "fgfgfgfgfg"),
+        Movie("Терминатор", "action", "120 min", 10.0, 1984, 1.0, 3.3, "fgfgfgfgfg"),
+        Movie("Терминатор2", "action", "120 min", 10.0, 1992, 1.4, 10.0, "fgfgfgfgfg"),
     )
     private var camedy = listOf(
-        Movie("Амерканский пирог", "action", "120 min",10.0, 1984, 1.0, 3.3, "fgfgfgfgfg"),
-        Movie("Американский пирог2", "action", "120 min",10.0, 1992, 1.4, 10.0, "fgfgfgfgfg"),
+        Movie("Амерканский пирог", "action", "120 min", 10.0, 1984, 1.0, 3.3, "fgfgfgfgfg"),
+        Movie("Американский пирог2", "action", "120 min", 10.0, 1992, 1.4, 10.0, "fgfgfgfgfg"),
     )
     private var triller = listOf(
-        Movie("Цвет ночи", "action", "120 min",10.0, 1984, 1.0, 3.3, "fgfgfgfgfg"),
-        Movie("Семь", "action", "120 min",10.0, 1992, 1.4, 10.0, "fgfgfgfgfg"),
+        Movie("Цвет ночи", "action", "120 min", 10.0, 1984, 1.0, 3.3, "fgfgfgfgfg"),
+        Movie("Семь", "action", "120 min", 10.0, 1992, 1.4, 10.0, "fgfgfgfgfg"),
     )
     private var ganre = listOf(
         FilmGenre("Боевик", action),
@@ -60,24 +57,23 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
     }
+
     fun setOotRecycledView() {
 
         val recyclerView: RecyclerView = findViewById(R.id.out_recycled_view)
-        //val onClickListener: MovieAdapter.OnStateClickListener
-        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false)
-        //recyclerView.adapter = OutAdapter(ganre,onClickListener.onStateClick())
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
         val stateClickListener: MovieAdapter.OnStateClickListener =
             object : MovieAdapter.OnStateClickListener {
                 override fun onStateClick(movie: Movie, position: Int) {
-                    val intent01 = Intent(this, FilmActivity::class.java)
-                    intent01.putExtra(Movie::class.java.getSimpleName(), movie)
-                    startActivity(intent01)
+                    val intent = Intent(this@MainActivity, FilmActivity::class.java)
+                    startActivity(intent)
                 }
             }
 
 
         recyclerView.adapter = OutAdapter(ganre, stateClickListener)
-
 
 
     }
@@ -90,14 +86,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.getItemId()){
-            R.id.action_search ->{
+        when (item.getItemId()) {
+            R.id.action_search -> {
                 Intent(this, SearchActivity::class.java)
                 startActivity(intent)
             }
-            R.id.custom ->{
+            R.id.custom -> {
                 val intent = Intent(this, SearchActivity::class.java)
-                startActivity(intent)}
+                startActivity(intent)
+            }
             R.id.about -> {
                 val intent = Intent(this, AboutActivity::class.java)
                 startActivity(intent)
