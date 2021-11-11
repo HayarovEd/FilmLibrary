3c1b4d295824036592bc4d45d37cd77397ec64d0
HayarovEd <edurd@rambler.ru>
2021-11-11 09:59:29 +0500

    transfer to filmActivity

--- app/src/main/java/com/edurda77/filmlibrary/ui/FilmActivity.kt
+++ app/src/main/java/com/edurda77/filmlibrary/ui/FilmActivity.kt
@@ -2,40 +2,48 @@ package com.edurda77.filmlibrary.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import com.edurda77.filmlibrary.R
 import com.edurda77.filmlibrary.domain.Movie
 
 class FilmActivity : AppCompatActivity() {
+    private var titlEditText: EditText? = null
+    private var ganreEditText: EditText? = null
+    private var durationEditText: EditText? = null
+    private var rangEditText: EditText? = null
+    private var yearEditText: EditText? = null
+    private var budgetEditText: EditText? = null
+    private var revenueEditText: EditText? = null
+    private var summaryEditText: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        val title : TextView? = findViewById (R.id.title_movie)
-        val rang : TextView? = findViewById (R.id.rang_movie)
-        val ganre : TextView? = findViewById (R.id.ganre_movie)
-        val year : TextView? = findViewById (R.id.year_movie)
-        val duration : TextView? = findViewById (R.id.duration_movie)
-        val budget : TextView? = findViewById (R.id.budget_movie)
-        val revenue : TextView? = findViewById (R.id.revenue_movie)
-        val summary : TextView? = findViewById (R.id.summary_movie)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film)
+        titlEditText = findViewById(R.id.title_movie)
+        ganreEditText = findViewById(R.id.ganre_movie)
+        durationEditText = findViewById(R.id.duration_movie)
+        rangEditText = findViewById(R.id.rang_movie)
+        yearEditText = findViewById(R.id.year_movie)
+        budgetEditText = findViewById(R.id.budget_movie)
+        revenueEditText = findViewById(R.id.revenue_movie)
+        summaryEditText = findViewById(R.id.summary_movie)
         val arguments = intent.extras
 
-        val movie: Movie?
+        val movie: Movie
         if (arguments != null) {
-            movie = arguments.getSerializable(Movie::class.java.getSimpleName()) as Movie?
-            if (movie != null) {
-                title?.setText(movie.movieTitle)
-                rang?.setText(movie.movieRang.toString())
-                ganre?.setText(movie.movieGanre)
-                year?.setText(movie.movieYear)
-                duration?.setText(movie.movieDuration)
-                budget?.setText(movie.movieBudget.toString())
-                revenue?.setText(movie.movieRevenue.toString())
-                summary?.setText(movie.movieSummary)
-            }
+            movie = arguments.getSerializable(Movie::class.java.getSimpleName()) as Movie
+            titlEditText?.setText(movie.movieTitle)
+            rangEditText?.setText(movie.movieRang.toString())
+            ganreEditText?.setText(movie.movieGanre)
+            yearEditText?.setText(movie.movieYear)
+            durationEditText?.setText(movie.movieDuration)
+            budgetEditText?.setText(movie.movieBudget.toString())
+            revenueEditText?.setText(movie.movieRevenue.toString())
+            summaryEditText?.setText(movie.movieSummary)
 
 
         }
-        setContentView(R.layout.activity_film)
+
 
     }
 }
