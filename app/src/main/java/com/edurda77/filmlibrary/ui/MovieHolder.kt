72bd135471ab42084cfaa17bd4f26c1ef6907f62
HayarovEd <edurd@rambler.ru>
2021-11-23 15:14:46 +0500

    load images in MainActivity

--- app/src/main/java/com/edurda77/filmlibrary/ui/MovieHolder.kt
+++ app/src/main/java/com/edurda77/filmlibrary/ui/MovieHolder.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.edurda77.filmlibrary.R
 import com.edurda77.filmlibrary.data.Movie
 import com.edurda77.filmlibrary.data.ResultSearchMovie
@@ -15,21 +16,26 @@ class MovieHolder (inflater: LayoutInflater, parent: ViewGroup) :
     private var titleMovie: TextView? = null
     private var idMovie: TextView? = null
     private var popularityMovie: TextView? = null
-    private var pictureMovie: ImageView? = null
+    private var pictureMovie: ImageView
+    private var beginURL = "https://image.tmdb.org/t/p/w500/"
 
 
     init {
         titleMovie = itemView.findViewById(R.id.title_movie)
         idMovie = itemView.findViewById(R.id.id_movie)
         popularityMovie = itemView.findViewById(R.id.popularity_movie)
-        pictureMovie = itemView.findViewById(R.id.picture_movie)
+        pictureMovie = itemView.findViewById(R.id.picture_movie_view)
     }
     @SuppressLint("SetTextI18n")
     fun bind(movie: ResultSearchMovie) {
         titleMovie?.text = movie.title
         idMovie?.text = idMovie?.text.toString()+movie.id.toString()
         popularityMovie?.text = popularityMovie?.text.toString() + movie.popularity
-        //pictureMovie?.setImageResource(23)
+        pictureMovie = itemView.findViewById(R.id.picture_movie_view)
+        Glide.with(this.itemView.context)
+            .load(beginURL+movie.posterPath)
+            .placeholder(R.drawable.video)
+            .into(pictureMovie)
 
     }
 
