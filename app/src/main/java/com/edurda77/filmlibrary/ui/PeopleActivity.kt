fdd98b0ca34200369b21d60d911d133cde24ed21
HayarovEd <edurd@rambler.ru>
2021-12-01 12:26:08 +0500

    make put data in PeopleActivity.kt

--- app/src/main/java/com/edurda77/filmlibrary/ui/PeopleActivity.kt
+++ app/src/main/java/com/edurda77/filmlibrary/ui/PeopleActivity.kt
@@ -1,27 +1,56 @@
 package com.edurda77.filmlibrary.ui
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
+import com.bumptech.glide.Glide
 import com.edurda77.filmlibrary.R
+import com.edurda77.filmlibrary.data.People
 import com.edurda77.filmlibrary.databinding.ActivityPeopleBinding
-import com.edurda77.filmlibrary.databinding.ActivitySearchBinding
 
 class PeopleActivity : AppCompatActivity() {
     private var toolbar: Toolbar? = null
+    private var beginURL = "https://image.tmdb.org/t/p/w500"
     private lateinit var binding: ActivityPeopleBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityPeopleBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setToolbar()
+        val nameText: TextView = binding.namePeople
+        val idText: TextView = binding.idPeople
+        val birthdayPeopleText: TextView = binding.birthdayPeople
+        val placeBirthPeopleText: TextView = binding.placeBirthPeople
+        val biographyPeopleText: TextView = binding.biographyPeople
+        val picture: ImageView = binding.picturePeople
+        val arguments = intent.extras
+        val people: People
+        if (arguments != null) {
+            people = arguments.getSerializable(People::class.java.simpleName) as People
+            nameText.text = people.namePeople
+            idText.text = idText.text.toString() + people.idPeoplePeople.toString()
+
+            birthdayPeopleText.text = birthdayPeopleText.text.toString() + people.birthdayPeople
+            placeBirthPeopleText.text = placeBirthPeopleText.text.toString() + people.placeBirthPeople
+            biographyPeopleText.text = biographyPeopleText.text.toString() + people.biographyPeople
+
+
+            Glide.with(this).load(beginURL + people.profilePathPeople)
+                //.override(320, 480)
+                .placeholder(R.drawable.video).into(picture)
+        }
         binding.requestGeolocation.setOnClickListener {
             checkPermission()
         }
@@ -68,16 +97,18 @@ class PeopleActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-    private fun checkPermission(){
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.
-            PERMISSION_GRANTED && checkSelfPermission(
-                Manifest.permission.
-            ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+    private fun checkPermission() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             TODO()
         } else {
-            ActivityCompat.requestPermissions(this, Array(2){
+            ActivityCompat.requestPermissions(this, Array(2) {
                 Manifest.permission.ACCESS_FINE_LOCATION
-                Manifest.permission.ACCESS_COARSE_LOCATION}, 1)
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 1)
         }
     }
 
@@ -92,7 +123,11 @@ class PeopleActivity : AppCompatActivity() {
 
 
         } else {
-            Toast.makeText(this,"Необходимо для определения места рождения актера на карте", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this,
+                "Необходимо для определения места рождения актера на карте",
+                Toast.LENGTH_LONG
+            ).show()
             //checkPermission()
         }
     }
